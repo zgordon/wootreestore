1b5d621f69acd5298b2763c33f2107c68bd9cdc2
Zac Gordon <email@zacgordon.com>
2015-07-15 00:06:02 -0400

    Initial upload of previous theme before WooCommerce additions

---
+++ functions.php
@@ -0,0 +1,92 @@
+<?php 
+
+add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'title-tag' );
+add_action( 'after_setup_theme', 'woocommerce_support' );
+
+add_filter('show_admin_bar', '__return_false');
+
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+function wpt_product_wrapper_start() {
+
+	if( is_product() ) require get_template_directory() . '/woocommerce/single-product/wrapper-start.php';
+	else require get_template_directory() . '/woocommerce/global/wrapper-shop-start.php';
+
+}
+add_action( 'woocommerce_before_main_content', 'wpt_product_wrapper_start' );
+
+function wpt_product_wrapper_end() {
+
+	require get_template_directory() . '/woocommerce/single-product/wrapper-end.php';
+;
+}
+add_action( 'woocommerce_after_main_content', 'wpt_product_wrapper_end' );
+
+
+
+function wpt_excerpt_length( $length ) {
+	return 16;
+}
+add_filter( 'excerpt_length', 'wpt_excerpt_length', 999 );
+
+function register_theme_menus() {
+
+	register_nav_menus(
+		array(
+			'primary-menu' 	=> __( 'Primary Menu', 'treehouse-portfolio' )			
+		)
+	);
+
+}
+add_action( 'init', 'register_theme_menus' );
+
+
+function wpt_create_widget( $name, $id, $description ) {
+
+	register_sidebar(array(
+		'name' => __( $name ),	 
+		'id' => $id, 
+		'description' => __( $description ),
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2 class="module-heading">',
+		'after_title' => '</h2>'
+	));
+
+}
+
+wpt_create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
+wpt_create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
+
+
+function wpt_theme_styles() {
+
+	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+	//wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'googlefont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+
+function wpt_theme_js() {
+
+	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );	
+	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
+	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );		
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+
+
+
+
+
+
+
+
+?>
---
+++ woocommerce/single-product/wrapper-start.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Content wrappers
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<section class="row">
+  <div class="small-12 columns text-center">
+    <div class="leader">    	
